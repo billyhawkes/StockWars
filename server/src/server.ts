cc1b4d738c35206e1a4875273b9d71ab11916ac0
Billy Hawkes <billyhawkes02@gmail.com>
2021-04-27 21:25:41 -0400

    Hosting Integrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://www.playstockwars.com/",
+    })
+);
 
 // ROUTES //
 app.get("/", (req, res) => {
